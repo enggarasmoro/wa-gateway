7f873fd3fd780be1ae59e0303702fe75d72fc609
enggarasmoro <enggarasmoro3@gmail.com>
2026-01-15 23:09:16 +0700

    bug fix send wa

--- src/services/whatsapp.service.ts
+++ src/services/whatsapp.service.ts
@@ -51,11 +51,6 @@ class WhatsAppService {
         ],
         timeout: 120000,
       },
-      // Web version cache for faster startup
-      webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
-      },
     });
 
     this.setupEventHandlers();
